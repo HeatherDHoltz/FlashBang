d1b554ccd72195bee047f67a2b8ec48d951646f0
HeatherDHoltz <heatherholtz2006@gmail.com>
2026-03-14 22:19:54 -0700

    Enhance homepage layout with increased window size and padding adjustments

--- src/main/java/cs151/application/Main.java
+++ src/main/java/cs151/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("homePage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400); // Increased window size
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
