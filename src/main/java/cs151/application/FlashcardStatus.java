ecec0e25d9ae4d47f199d9ab22760311b86aa0bb
HeatherDHoltz <heatherholtz2006@gmail.com>
2026-03-28 23:36:03 -0700

    Add FlashcardStatus enum to represent flashcard lifecycle

---
+++ src/main/java/cs151/application/FlashcardStatus.java
@@ -0,0 +1,12 @@
+package cs151.application;
+
+/**
+ * Enum representing the lifecycle/status of a flashcard.
+ * Stored as the enum name in the flashcards file (FlashcardStore).
+ */
+public enum FlashcardStatus {
+    NEW,
+    LEARNING,
+    MASTERED
+}
+
